5718c767f8a430a350fdb70be229e341fa3ce406
Kweku Moses <kweku.moses@hotmail.com>
2021-05-04 21:23:46 +0200

    fix url

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ class App
     
     public static function getData()
     {
-        $json = file_get_contents("data.json");
+        $json = file_get_contents("fakeStore/data.json");
         if (!$json)
             throw new Exception("Could not access URL");
         return json_decode($json, true);
@@ -32,7 +32,7 @@ class App
     
     public static function getErrorMessage()
     {
-        $json = file_get_contents("error.json");
+        $json = file_get_contents("fakeStore/error.json");
         if (!$json)
             throw new Exception("Could not access URL");
         return json_decode($json, true);
