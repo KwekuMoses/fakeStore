54b4017fb4a685d049dedf5b10a878d5fa199d9d
Kweku Moses <kweku.moses@hotmail.com>
2021-05-04 19:13:55 +0200

    will fix category filter

--- index.php
+++ index.php
@@ -2,81 +2,80 @@
 include_once "functions.php";
 
 
-class App {
-
-
-
-
+class App
+{
+    
+    
+    
     public static function main()
     {
         try {
-            $array = self::getData();
+            $array        = self::getData();
             $errormessage = self::getErrorMessage();
             self::viewData($array, $errormessage);
-        } catch (Exception $error) {
+        }
+        catch (Exception $error) {
             echo $error->getMessage();
         }
     }
-
-
-
-
-public static function getData()
+    
+    
+    
+    
+    public static function getData()
     {
         $json = file_get_contents("http://localhost/PHP/Inl%C3%A4mning-2/fakeStore/data.json");
-
+        
         if (!$json)
             throw new Exception("Could not access URL");
         return json_decode($json, true);
     }
-
+    
     public static function getErrorMessage()
     {
         $json = file_get_contents("http://localhost/PHP/Inl%C3%A4mning-2/fakeStore/error.json");
-
+        
         if (!$json)
             throw new Exception("Could not access URL");
         return json_decode($json, true);
     }
-
-
+    
+    
     public static function viewData($array, $errormessage)
     {
-   /*    if show = 18 visa 18
-       if category = jewelry visa bara jewelry
-       if show = 100 välj en mindre siffra
-       if category = jew kategorin existerar inte */
-       if (isset($_GET['show'])) { 
-        $show = $_GET["show"];
-        if ($show < 20)
-        for ($i = 1; $i < $show; $i++) {
-         print_array($array[$i - 1]);
-       }   
-       else if ($show > 20) {
-        print_array($errormessage[0]);
-       }
-       else if ($)
-    }
-
-  else if (isset($_GET['category'])) { 
-        $category = $_GET['category'];
-        foreach ($array as $product) {
-            if ($product["category"] == $category) {
-                print_array($product);
+        if (isset($_GET['show'])) {
+            $show = $_GET["show"];
+            if ($show < 20)
+                for ($i = 0; $i < $show; $i++) {
+                    print_array($array[$i]);
+                } else if ($show > 20) {
+                print_array($errormessage[0]);
             }
-          }   
-
-
-       
-       
-    }
-    else {
-        print_array($array);
-    }
-
-   
-
         }
+
+        
+        if (isset($_GET['category'])) {
+            $category = $_GET['category'];
+            foreach ($array as $product) {
+                if ($product['category'] == $category) {
+                    print_array($product);
+                }
+              
+            }
+            if ($category) {
+                print_array($errormessage[1]);
+                exit(0);
+            }
+        
+        }
+        
+        else {
+            print_array($array);
+        }
+        
+        
+        
+    }
 }
 
 App::main();
@@ -89,9 +88,9 @@ JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 $show = $_GET["show"];
 
 if ($show < 18) {
-    echo "Too young";
+echo "Too young";
 } else {
-    echo "welcome old one";
+echo "welcome old one";
 }
 
 */
