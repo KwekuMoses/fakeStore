aeb0aca0748c290f6a12a78b193a1cf9fee2f5b2
Kweku Moses <kweku.moses@hotmail.com>
2021-05-05 13:57:23 +0200

    working but not filtered JSON

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-include_once "functions.php";
-
+//include_once "functions.php";
+header('Content-Type: application/json');
 
 class App
 {
@@ -27,7 +27,7 @@ class App
 
         if (!$json)
             throw new Exception("Could not access URL");
-        return $json;
+        return json_decode($json, true);
     }
 
     public static function getErrorMessage()
@@ -44,9 +44,10 @@ class App
     {
 
         if ($_GET == null) {
-            print_array($array);
-            echo "där";
+            $json_string = json_encode($array, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+            echo $json_string;
         }
+
         $allowed_key_1 = isset($_GET['show']);
         $allowed_key_2 = isset($_GET['category']);
         if ($allowed_key_1 || $allowed_key_2) {
@@ -57,8 +58,10 @@ class App
 
                     for ($i = 0; $i < $show; $i++) {
                         $random_number = (rand(0, 20));
-                        print_array($array[$random_number]);
+                        $json_string = json_encode($array[$random_number], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+                        echo $json_string;
                     }
+
                 else if ($show > $array_count) {
                     print_array($errormessage[0]);
                 }
@@ -67,8 +70,8 @@ class App
 
             if (isset($_GET['category'])) {
                 $category = $_GET['category'];
-
                 $print_these = array();
+
 
                 foreach ($array as $product) {
                     if ($product['category'] == $category) {
@@ -76,8 +79,9 @@ class App
                         array_push($print_these, $product);
 
                         if (count($print_these) > 0) {
-                            print_array($product);
-                            echo count($print_these);
+                            //    print_r($product);
+                            $json_string = json_encode($product, JSON_PRETTY_PRINT);
+                            echo $json_string;
                         }
                     }
                 }
